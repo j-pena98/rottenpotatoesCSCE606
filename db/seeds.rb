33fb5cd953547eec3a5e81473d8623de41a208ee
Jasmine Pena <j-pena98@github.com>
2025-02-11 21:25:31 -0600

    adding more movies to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Ratatouille', :rating => 'G',
+    :release_date => '29-Jun-2007'},
+  {:title => 'Aladdin', :rating => 'G',
+    :release_date => '25-Nov-1992'},
+  {:title => 'Scream', :rating => 'R',
+    :release_date => '20-Dec-1996'}
 ]
 
 more_movies.each do |movie|
